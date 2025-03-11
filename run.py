79987eb6ea1d8e4a368b3d53b82b4e3050e6f3cb
Manookian <bmanookian@coh.org>
2025-03-11 14:00:36 -0700

    adding run.py file

--- run.py
+++ run.py
@@ -3,17 +3,32 @@ sys.path.append('/home/bmanookian/rescore/')
 import rescore_BN as rBN
 
 ## User Inputs ##
-files=['Rep1.csv','Rep2.csv','Rep3.csv']
-lengths=[100,100,100]
+
+# If you have a set of trajectories. Provide the names and the lengths of the trajs
+files=None    #['Rep1.csv','Rep2.csv','Rep3.csv']
+lengths=None   #[100,100,100]
+
+# If you have a single file that has already combned the trajectroies use filename
+filename='filename.csv'
+
+# Provide the .dot file for your BN universal graph
 dotfile='./graph.dot'
+
+# Data 'discrete' or 'continuous'
 datatype='continuous'
-ncore=20
+
+# Output file for rescore values
 outfile='out.csv'
+
+# nuber of cores
+ncore=20
 ##
 
 
 ## Run codes ##
-Rescore=rBN.BN_Rescore(dotfile=dotfile,lengths=lengths,files=files)
+# Use the folloiwng for sets of trajectories
+Rescore=rBN.BN_Rescore(dotfile=dotfile,files=files,data=filename,lengths=lengths)
 Rescore.rescore(datatype=datatype,ncore=ncore)
 Rescore.table_write(outfile)
+
 ##
